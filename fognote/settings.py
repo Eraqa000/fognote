d396d278b48bd602e6832e6d146f4d1f30531b30
Eraqa000 <sarsebaiernar@gmail.com>
2025-11-23 22:00:06 +0500

    Prepare for Render deploy

--- fognote/settings.py
+++ fognote/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-w75z9!%d2dd#*i08$4oi0zg*5v0%h=k@)-2va8=^*&2(nnxses"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DJANGO_DEBUG", "True").lower() == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".onrender.com"]
 
 
 # Application definition
